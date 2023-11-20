c5086b412d1864569b1b8a3648ce2894565e4e14
MrRoik <138239220+MrRoik@users.noreply.github.com>
2023-11-20 20:57:17 +0200

    task 3 - js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -19,5 +19,35 @@ import throttle from 'lodash.throttle';
 const LS_KEY = "feedback-form-state";
 
 const form = document.querySelector(".feedback-form");
-form.addEventListener("input", throttle(storageFormData, 500));
-form.addEventListener("submit", clickFormSubmit);
+form.addEventListener("input", throttle(onInputData, 500));
+form.addEventListener("submit", onFormSubmit);
+
+
+let dataForm = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+const { email, message } = form.elements;
+descFeedbackForm();
+
+function onInputData() {
+   dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LS_KEY, JSON.stringify(dataForm));
+}
+
+function descFeedbackForm() {
+  if (dataForm) {
+    email.value = dataForm.email || "";
+    message.value = dataForm.message || "";
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === "" || message.value === "") {
+    return alert("Ви не заповнили потрібні поля!!! Будь ласка, заповніть усі поля!");
+  }
+
+  localStorage.removeItem(LS_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
